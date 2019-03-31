bfd124a90b8681754f777d53bf9dd45c96c12426
max-prosper <maxim.yaaman@gmail.com>
2019-03-31 17:05:14 +0300

    Minor fix

--- tcpserver/simple_tcp.c
+++ tcpserver/simple_tcp.c
@@ -10,48 +10,48 @@
 #define PORT 8080
 
 int main(void) {
-	int sockfd, new_sockfd;
-	struct sockaddr_in host_addr, client_addr;
-	socklen_t sin_size;
-	int recv_length=1, yes=1;
-	unsigned char buffer[1024];
+    int sockfd, new_sockfd;
+    struct sockaddr_in host_addr, client_addr;
+    socklen_t sin_size;
+    int recv_length=1, yes=1;
+    unsigned char buffer[1024];
     char welcome_phrase[9]="WELCOME!\n";
 
-	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1)
-		fatal("in socket");
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1)
+        fatal("in socket");
 
-	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
-		fatal("setting socket option SO_REUSEADDR");
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+        fatal("setting socket option SO_REUSEADDR");
 
-	host_addr.sin_family = AF_INET;
-	host_addr.sin_port = htons(PORT);
-	host_addr.sin_addr.s_addr = INADDR_ANY;
-	memset(&(host_addr.sin_zero), '\0', 8);
+    host_addr.sin_family = AF_INET;
+    host_addr.sin_port = htons(PORT);
+    host_addr.sin_addr.s_addr = INADDR_ANY;
+    memset(&(host_addr.sin_zero), '\0', 8);
 
-	if (bind(sockfd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr)) == -1)
-		fatal("binding to socket");
+    if (bind(sockfd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr)) == -1)
+        fatal("binding to socket");
 
-	if (listen(sockfd, 5) == -1)
-		fatal("listening on socket");
+    if (listen(sockfd, 5) == -1)
+        fatal("listening on socket");
 
-	while(1) {
-		sin_size = sizeof(struct sockaddr_in);
-		new_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &sin_size);
+    while(1) {
+        sin_size = sizeof(struct sockaddr_in);
+        new_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &sin_size);
 
-		if(new_sockfd == -1)
-			fatal("accepting connection");
-		printf("SERVER: got connection from %s port %d\n",inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        if(new_sockfd == -1)
+            fatal("accepting connection");
+        printf("SERVER: got connection from %s port %d\n",inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-		send(new_sockfd, &welcome_phrase, strlen(welcome_phrase), 0);
-		recv_length = recv(new_sockfd, &buffer, 1024, 0);
+        send(new_sockfd, &welcome_phrase, strlen(welcome_phrase), 0);
+        recv_length = recv(new_sockfd, &buffer, 1024, 0);
 
-		while(recv_length > 0) {
-			printf("RECEIVED: %d bytes\n", recv_length);
-			dump(buffer, recv_length);
-			recv_length = recv(new_sockfd, &buffer, 1024, 0);
-		}
-		close(new_sockfd);
-	}
-	return 0;
+        while(recv_length > 0) {
+            printf("RECEIVED: %d bytes\n", recv_length);
+            dump(buffer, recv_length);
+            recv_length = recv(new_sockfd, &buffer, 1024, 0);
+        }
+        close(new_sockfd);
+    }
+    return 0;
 }
 
