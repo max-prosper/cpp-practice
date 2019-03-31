bda698bf8b315b0152c8b0c09894a8fcbf09e1d1
max-prosper <maxim.yaaman@gmail.com>
2019-03-31 16:31:55 +0300

    Simple TCP server example

---
+++ tcpserver/simple_tcp.h
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void fatal(char *message) {
+   char error_message[100];
+
+   strcpy(error_message, "[!!] Fatal Error ");
+   strncat(error_message, message, 83);
+   perror(error_message);
+   exit(-1);
+}
+
+void *ec_malloc(unsigned int size) {
+   void *ptr;
+   ptr = malloc(size);
+
+   if(ptr == NULL)
+      fatal("in ec_malloc() on memory allocation");
+
+   return ptr;
+}
+
+// prints memomy dump in hex with with delimiters
+void dump(const unsigned char *buffer, const unsigned int length) {
+	unsigned char byte;
+	unsigned int i, j;
+
+	for(i=0; i < length; i++) {
+		byte = buffer[i];
+		printf("%02x ", buffer[i]);
+
+		if(((i%16)==15) || (i==length-1)) {
+			for(j=0; j < 15-(i%16); j++)
+				printf("   ");
+			printf("| ");
+
+			for(j=(i-(i%16)); j <= i; j++) {
+				byte = buffer[j];
+				if((byte > 31) && (byte < 127))
+					printf("%c", byte);
+				else
+					printf(".");
+			}
+
+			printf("\n");
+		}
+	}
+}
+
